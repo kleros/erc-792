1dc2d8eb1d19e491afc34e60cb0a95fdd7e09cb3
Ferit Tunçer <ferit@cryptolab.net>
2019-05-07 14:02:03 +0300

    feat(erc-792): initial

---
+++ truffle.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  compilers: {
+    solc: {
+      version: '^0.5.8'
+    }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD'
+    }
+  },
+
+  networks: {
+    development: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8545
+    },
+    kovan: {
+      confirmations: 2,
+      gas: 4200000,
+      gasPrice: 20000000000,
+      network_id: 42
+    }
+  }
+}
