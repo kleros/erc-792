c00f37dacdbf296e038bbaec9ad86c6a2f4b48d1
Ferit Tunçer <ferit@cryptolab.net>
2019-11-20 18:59:26 +0300

    refactor(IArbitrator): contract IArbitrator to interface IArbitrator

--- contracts/IArbitrator.sol
+++ contracts/IArbitrator.sol
@@ -14,7 +14,7 @@ import "./IArbitrable.sol";
  *  -Define the functions for cost display (arbitrationCost and appealCost).
  *  -Allow giving rulings. For this a function must call arbitrable.rule(disputeID, ruling).
  */
-contract IArbitrator {
+interface IArbitrator {
 
     enum DisputeStatus {Waiting, Appealable, Solved}
 
@@ -42,43 +42,43 @@ contract IArbitrator {
      *  @param _extraData Can be used to give additional info on the dispute to be created.
      *  @return disputeID ID of the dispute created.
      */
-    function createDispute(uint _choices, bytes memory _extraData) public payable returns(uint disputeID);
+    function createDispute(uint _choices, bytes calldata _extraData) external payable returns(uint disputeID);
 
     /** @dev Compute the cost of arbitration. It is recommended not to increase it often, as it can be highly time and gas consuming for the arbitrated contracts to cope with fee augmentation.
      *  @param _extraData Can be used to give additional info on the dispute to be created.
      *  @return cost Amount to be paid.
      */
-    function arbitrationCost(bytes memory _extraData) public view returns(uint cost);
+    function arbitrationCost(bytes calldata _extraData) external view returns(uint cost);
 
     /** @dev Appeal a ruling. Note that it has to be called before the arbitrator contract calls rule.
      *  @param _disputeID ID of the dispute to be appealed.
      *  @param _extraData Can be used to give extra info on the appeal.
      */
-    function appeal(uint _disputeID, bytes memory _extraData) public payable;
+    function appeal(uint _disputeID, bytes calldata _extraData) external payable;
 
     /** @dev Compute the cost of appeal. It is recommended not to increase it often, as it can be higly time and gas consuming for the arbitrated contracts to cope with fee augmentation.
      *  @param _disputeID ID of the dispute to be appealed.
      *  @param _extraData Can be used to give additional info on the dispute to be created.
      *  @return cost Amount to be paid.
      */
-    function appealCost(uint _disputeID, bytes memory _extraData) public view returns(uint cost);
+    function appealCost(uint _disputeID, bytes calldata _extraData) external view returns(uint cost);
 
     /** @dev Compute the start and end of the dispute's current or next appeal period, if possible. If not known or appeal is impossible: should return (0, 0).
      *  @param _disputeID ID of the dispute.
      *  @return The start and end of the period.
      */
-    function appealPeriod(uint _disputeID) public view returns(uint start, uint end);
+    function appealPeriod(uint _disputeID) external view returns(uint start, uint end);
 
     /** @dev Return the status of a dispute.
      *  @param _disputeID ID of the dispute to rule.
      *  @return status The status of the dispute.
      */
-    function disputeStatus(uint _disputeID) public view returns(DisputeStatus status);
+    function disputeStatus(uint _disputeID) external view returns(DisputeStatus status);
 
     /** @dev Return the current ruling of a dispute. This is useful for parties to know if they should appeal.
      *  @param _disputeID ID of the dispute.
      *  @return ruling The ruling which has been given or the one which will be given if there is no appeal.
      */
-    function currentRuling(uint _disputeID) public view returns(uint ruling);
+    function currentRuling(uint _disputeID) external view returns(uint ruling);
 
 }
